0cda18168bc4157b0835a7c834f24dfe1fa1dd34
sagarpathak0 <112740255+sagarpathak0@users.noreply.github.com>
2024-07-14 17:03:36 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -4,7 +4,6 @@ import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { useState } from "react";
 import { handleError, handleSuccess } from "../utils";
-import 'font-awesome/css/font-awesome.min.css';
 
 function Login() {
   const [loginInfo, setLoginInfo] = useState({
@@ -63,9 +62,9 @@ function Login() {
         <h2>Welcome back</h2>
         <p>Please enter your details to sign in.</p>
         <div className="social-login">
-          <button className="social-button apple"><i className="fa fa-apple"></i> Apple</button>
-          <button className="social-button google"><i className="fa fa-google"></i> Google</button>
-          <button className="social-button twitter"><i className="fa fa-twitter"></i> Twitter</button>
+          <button className="social-button apple">Apple</button>
+          <button className="social-button google">Google</button>
+          <button className="social-button twitter">Twitter</button>
         </div>
         <p className="or">OR</p>
         <form onSubmit={handleLogin}>
