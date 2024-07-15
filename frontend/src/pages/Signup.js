e07f829b28d01657ff61d0dc0f93b2976e155509
sagarpathak0 <112740255+sagarpathak0@users.noreply.github.com>
2024-07-15 09:32:29 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -27,7 +27,8 @@ function Signup() {
       return handleError("Name, email, and password are required");
     }
     try {
-      const url = "http://localhost:8080/auth/signup";
+      const url = "https://tracker-money-api.vercel.app/auth/signup";
+      
       const response = await fetch(url, {
         method: "POST",
         headers: {
